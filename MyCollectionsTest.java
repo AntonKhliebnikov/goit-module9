6c45c2ba7e65cad949d268ddc52bbeb05e4ca078
AntonKhliebnikov <towulik@gmail.com>
2022-10-17 23:55:20 +0300

    homework module9 add MyLinkedList

--- MyCollectionsTest.java
+++ MyCollectionsTest.java
@@ -1,26 +1,66 @@
 package module9;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MyCollectionsTest {
     public static void main(String[] args) {
-        MyArrayList<String> strArr = new MyArrayList<>();
-        strArr.add("66");
-        strArr.add("24");
-        strArr.add("1");
-        strArr.add("6");
-        strArr.add("94");
-        strArr.add("33");
-        strArr.add("17");
-        System.out.println("strArr.size() = " + strArr.size());
-        System.out.println("strArr = " + strArr);
-        System.out.println("strArr.get(3) = " + strArr.get(3));
+//        MyArrayList<String> strArr = new MyArrayList<>();
+//        strArr.add("66");
+//        strArr.add("24");
+//        strArr.add("1");
+//        strArr.add("6");
+//        strArr.add("94");
+//        strArr.add("33");
+//        strArr.add("17");
+//        System.out.println("strArr.size() = " + strArr.size());
+//        System.out.println("strArr = " + strArr);
+//        System.out.println("strArr.get(3) = " + strArr.get(3));
+//
+//        System.out.println("strArr.remove(3) = " + strArr.remove(3));
+//        System.out.println("strArr = " + strArr);
+//        System.out.println("strArr.size() = " + strArr.size());
+//
+//        System.out.println("strArr.get(3) = " + strArr.get(3));
+//
+//        strArr.clear();
+//        System.out.println("strArr = " + strArr);
 
-        System.out.println("strArr.remove(3) = " + strArr.remove(3));
-        System.out.println("strArr = " + strArr);
-        System.out.println("strArr.size() = " + strArr.size());
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        list.add(22);
+        list.add(15);
+        list.add(54);
+        list.print();
+        System.out.println("list.size = " + list.size());
 
-        System.out.println("strArr.get(3) = " + strArr.get(3));
-        
-        strArr.clear();
-        System.out.println("strArr = " + strArr);
+        list.remove(0);
+        list.print();
+        System.out.println("list.size = " + list.size());
+
+        list.add(65);
+        list.print();
+        System.out.println("list.size = " + list.size());
+
+        list.remove(2);
+        list.print();
+        System.out.println("list.size = " + list.size());
+
+        list.remove(0);
+        list.print();
+        System.out.println("list.size = " + list.size());
+
+        list.clear();
+        System.out.println("list.size = " + list.size());
+
+        list.add(698);
+        list.add(69);
+        list.add(121);
+        list.add(1777);
+        list.add(193);
+        list.print();
+        System.out.println("list.size = " + list.size());
+        System.out.println("list.get(3) = " + list.get(1));
+
+
     }
 }
---
+++ MyLinkedList.java
@@ -0,0 +1,90 @@
+package module9;
+
+import java.util.Objects;
+
+public class MyLinkedList<T> {
+    private int size;
+    private Node<T> first;
+    private Node<T> last;
+
+    public MyLinkedList() {
+        first = null;
+        last = null;
+    }
+
+    public static class Node<T> {
+        T element;
+        Node<T> next;
+        Node<T> prev;
+
+        public Node(T element) {
+            this.element = element;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "element=" + element +
+                    '}';
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(T element) {
+        Node<T> newNode = new Node<>(element);
+        if (first == null) {
+            first = last = newNode;
+        } else {
+            last.next = newNode;
+        }
+
+        newNode.prev = last;
+        last = newNode;
+        size++;
+    }
+
+    public void clear() {
+        first = last = null;
+        size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> currentNode = first;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return (T) currentNode;
+    }
+
+    public void remove(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> currentNode = first;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+
+        if (first == currentNode) {
+            first = first.next;
+        }
+
+        if (last == currentNode) {
+            last = last.prev;
+        }
+        currentNode.prev.next = currentNode.next;
+        size--;
+
+    }
+
+    public void print() {
+        Node<T> newNode = first;
+        while (newNode != null) {
+            System.out.println(newNode.element);
+            newNode = newNode.next;
+        }
+    }
+}
