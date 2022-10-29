eccfbb479fa7884d17cdc806d2e484804968da39
AntonKhliebnikov <towulik@gmail.com>
2022-10-29 23:18:34 +0300

    homework module9 fix MyHashMap

--- MyCollectionsTest.java
+++ MyCollectionsTest.java
@@ -2,16 +2,54 @@ package module9;
 
 public class MyCollectionsTest {
     public static void main(String[] args) {
+        MyHashMap<String, Integer> hashMap = new MyHashMap<>();
 
-        MyLinkedList<String> linkedList = new MyLinkedList<>();
-        linkedList.add("next0");
-        linkedList.add("next1");
-        linkedList.add("next2");
-        linkedList.add("next3");
-        System.out.println("linkedList.size() = " + linkedList.size());
-        linkedList.remove(2);
-        System.out.println("linkedList.size() = " + linkedList.size());
-        linkedList.print();
-        System.out.println("linkedList.get(0) = " + linkedList.get(0));
+        hashMap.put("Anton", 36);
+        hashMap.put("Kris", 26);
+        hashMap.put("Polina", 38);
+        hashMap.put("Anfisa", 38);
+        hashMap.put("Tanya", 33);
+        hashMap.put("Olya", 23);
+        hashMap.put("Ira", 27);
+        hashMap.put("Katya", 27);
+        hashMap.put("Alyona", 30);
+        hashMap.put("Yana", 23);
+        hashMap.put("Masha", 33);
+        hashMap.put("Anya", 39);
+        hashMap.put("Tolik", 36);
+        hashMap.put("Oksana", 40);
+        hashMap.put("Vera", 30);
+        hashMap.put("Sasha", 23);
+        hashMap.put("Kolya", 28);
+        hashMap.put("Anton", 28);
+        hashMap.remove("Anton");
+        hashMap.remove("Sasha");
+        hashMap.remove("Anfisa");
+        hashMap.remove("Katya");
+
+//        System.out.println("hashMap.hash(\"Oksana\") = " + hashMap.hash("Anton"));
+//        System.out.println("hashMap.hash(\"Sasha\") = " + hashMap.hash("Sasha"));
+//        System.out.println("hashMap.hash(\"Vera\") = " + hashMap.hash("Vera"));
+//        System.out.println("hashMap.size() = " + hashMap.size());
+//        hashMap.remove("Yana");
+        System.out.println("hashMap.size() = " + hashMap.size());
+        System.out.println("hashMap = " + hashMap);
+        hashMap.print();
+//        System.out.println("hashMap.get(\"Anya\") = " + hashMap.get("Anya"));
+//        System.out.println("hashMap.get(\"Katya\") = " + hashMap.get("Katya"));
+//        System.out.println("hashMap.get(\"Oksana\") = " + hashMap.get("Oksana"));
+//        System.out.println("hashMap.get(\"Anton\") = " + hashMap.get("Anton"));
+//        System.out.println("hashMap.get(\"Anya\") = " + hashMap.get("Anya"));
+//        System.out.println("hashMap.get(\"Oksana\") = " + hashMap.get("Oksana"));
+//        System.out.println("hashMap.hash(\"Katya\") = " + hashMap.hash("Katya"));
+//        System.out.println("hashMap.hash(\"Anya\") = " + hashMap.hash("Anya"));
+//        System.out.println("hashMap.hash(\"Oksana\") = " + hashMap.hash("Oksana"));
+//        System.out.println("hashMap.hash(\"Kolya\") = " + hashMap.hash("Kolya"));
+//        System.out.println("hashMap.hash(\"Polina\") = " + hashMap.hash("Polina"));
+//        System.out.println("hashMap.hash(\"Olya\") = " + hashMap.hash("Olya"));
+//        System.out.println("hashMap.hash(\"Masha\") = " + hashMap.hash("Masha"));
+//        hashMap.clear();
+//        System.out.println("hashMap = " + hashMap);
+//        System.out.println("hashMap.size() = " + hashMap.size());
     }
 }
--- MyHashMap.java
+++ MyHashMap.java
@@ -1,8 +1,24 @@
 package module9;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class MyHashMap<K, V> {
-    private Node<K, V> element;
+    private static final int DEFAULT_CAPACITY = 16;
+    private Node<K, V>[] table;
     private int size;
+
+    @SuppressWarnings("unchecked")
+    public MyHashMap(int initCapacity) {
+        if (initCapacity <= 0) {
+            throw new IllegalArgumentException();
+        }
+        table = (Node<K, V>[]) new Node<?, ?>[initCapacity];
+    }
+
+    public MyHashMap() {
+        this(DEFAULT_CAPACITY);
+    }
 
     public static class Node<K, V> {
         K key;
@@ -16,46 +32,82 @@ public class MyHashMap<K, V> {
 
         @Override
         public String toString() {
-            return "Node{" +
-                    "key=" + key +
-                    ", value=" + value +
-                    '}';
+            return key + "=" + value;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Node<?, ?> node)) return false;
+            return key.equals(node.key) && value.equals(node.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, value);
+        }
+    }
+
+    public int hash(K key) {
+        int hash;
+        hash = key.hashCode() % table.length;
+        if (hash < 0) {
+            hash *= -1;
+        }
+        return hash;
     }
 
     public int size() {
         return size;
     }
-
+    
     public void put(K key, V value) {
         Node<K, V> newNode = new Node<>(key, value);
-        Node<K, V> currentNode = element;
-        if (element == null) {
-            element = newNode;
-        } else {
-            while (currentNode.next != null) {
-                if (currentNode.key.equals(key)) {
-                    currentNode.value = value;
-                    return;
+        Node<K, V> currentNode;
+        int index = 0;
+        for (Node<K, V> ignored : table) {
+            if (hash(key) == index) {
+                if (table[index] == null) {
+                    table[index] = newNode;
+                    break;
+                } else {
+                    currentNode = table[index];
+                    while (currentNode.next != null) {
+                        if (currentNode.key.equals(key)) {
+                            currentNode.value = value;
+                            return;
+                        }
+                        currentNode = currentNode.next;
+                    }
+
+                    if (currentNode.key.equals(key)) {
+                        currentNode.value = value;
+                        return;
+                    }
+                    currentNode.next = newNode;
                 }
-                currentNode = currentNode.next;
             }
-
-            if (currentNode.key.equals(key)) {
-                currentNode.value = value;
-                return;
-            }
-
-            currentNode.next = newNode;
+            index++;
         }
         size++;
     }
 
+    @SuppressWarnings("ReassignedVariable")
     public void remove(K key) {
-        Node<K, V> currentNode = element;
-        Node<K, V> prevNode = element;
-        int index = 0;
+        Node<K, V> currentNode = null;
+        Node<K, V> prevNode = null;
+        int indexOfTableElements = 0;
+        for (Node<K, V> ignored : table) {
+            if (hash(key) == indexOfTableElements) {
+                currentNode = table[indexOfTableElements];
+                prevNode = table[indexOfTableElements];
+                break;
+            }
+            indexOfTableElements++;
+            System.out.println("indexOfTableElements = " + indexOfTableElements);
+        }
 
+        int index = 0;
         while (currentNode != null) {
             if (currentNode.key.equals(key)) {
                 break;
@@ -69,8 +121,9 @@ public class MyHashMap<K, V> {
         }
 
         if (index == 0) {
-            assert element != null;
-            element = element.next;
+            assert prevNode != null;
+            table[indexOfTableElements] = prevNode.next;
+            System.out.println("prevNode = " + prevNode);
         } else if (prevNode.next != null) {
             prevNode.next = prevNode.next.next;
         }
@@ -78,27 +131,37 @@ public class MyHashMap<K, V> {
     }
 
     public void clear() {
-        element = null;
+        table = null;
         size = 0;
     }
 
     public V get(K key) {
-        Node<K, V> newNode = element;
-        V value = newNode.value;
-        while (newNode != null) {
-            if (newNode.key.equals(key)) {
-                value = newNode.value;
+        V value = null;
+        for (Node<K, V> element : table) {
+            Node<K, V> newNode = element;
+            while (newNode != null) {
+                if (newNode.key.equals(key)) {
+                    value = newNode.value;
+                }
+                newNode = newNode.next;
             }
-            newNode = newNode.next;
         }
         return value;
     }
 
     public void print() {
-        Node<K, V> newNode = element;
-        while (newNode != null) {
-            System.out.println(newNode.key + ": " + newNode.value);
-            newNode = newNode.next;
+        int index = 0;
+        for (Node<K, V> ignored : table) {
+            if (table[index] != null) {
+                if (index == hash(table[index].key)) {
+                    Node<K, V> newNode = table[index];
+                    while (newNode != null) {
+                        System.out.print("[" + newNode.key + "=" + newNode.value + "], ");
+                        newNode = newNode.next;
+                    }
+                }
+            }
+            index++;
         }
     }
 }
